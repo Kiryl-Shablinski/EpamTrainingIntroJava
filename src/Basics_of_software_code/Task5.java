239b78e3c1f2458df2a160ed25d3aba84265d154
Kiryl Shablinski <kirya-0029@mail.ru>
2022-02-14 21:26:02 +0300

    Task5

---
+++ src/Basics_of_software_code/Task5.java
@@ -0,0 +1,17 @@
+package Basics_of_software_code;
+
+
+public class Task5 {
+    public static void main(String[] args) {
+        int time = 3987;
+
+        System.out.println(getTime(time));
+    }
+    public static String getTime(int time){
+        int seconds = time % 60;
+        int  mints = (time / 60) % 60;
+        int hours = (time / 3600);
+
+        return String.format("%dч %dмин %dс.",hours, mints, seconds );
+    }
+}
