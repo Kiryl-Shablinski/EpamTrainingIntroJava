6a36e610bff584354eb73153865f5356985b84d2
Kiryl Shablinski <kirya-0029@mail.ru>
2022-02-14 21:36:04 +0300

    Task3

--- src/Basics_of_software_code/Task3.java
+++ src/Basics_of_software_code/Task3.java
@@ -6,17 +6,11 @@ public class Task3 {
     public static void main(String[] args) {
         //(sin(x) + cos(y)) / (cos(x) - sin(y)) * tan(x * y);
 
-        System.out.printf("%.12f",getFun(Math.toRadians(45),Math.toRadians(15)));
+        System.out.println(getFun(Math.toRadians(45),Math.toRadians(15)));
     }
 
     public static double getFun(double x, double y){
-     /*  System.out.printf("%.12f\n",(Math.sin(x) + Math.cos(y)));
-        System.out.printf("%.12f\n",Math.cos(x) - Math.sin(y));
-        System.out.printf("%.12f\n",(Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y)));
-        System.out.printf("%.12f\n",Math.sin(x * y));
-        System.out.printf("%.12f\n",Math.cos(x * y));
 
-      */
         return (Math.sin(x) + Math.cos(y)) / (Math.cos(x) - Math.sin(y)) * Math.tan(x * y);
     }
 }
