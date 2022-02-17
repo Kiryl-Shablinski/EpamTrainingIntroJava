f01ce69a07af82777635a92c406dc05c75681b60
Kiryl Shablinski <kirya-0029@mail.ru>
2022-02-17 22:57:25 +0300

    Task2.3

--- src/Basics_of_software_code/branching/Task3.java
+++ src/Basics_of_software_code/branching/Task3.java
@@ -2,29 +2,33 @@ package Basics_of_software_code.branching;
 
 public class Task3 {
     public static void main(String[] args) {
-        Point pointA = new Point(4,6);
-        Point pointB = new Point(6,9);
-        Point pointC = new Point(3,10);
+        Task3 solution = new Task3();
+        Point pointA = solution.new Point(4,6);
+        Point pointB = solution.new Point(6,9);
+        Point pointC = solution. new Point(3,10);
 
-        if (pointToLine(pointA.x, pointA.y, pointB.x, pointB.y, pointC.x, pointC.y)){
+        if (pointToLine(pointA.x, pointA.y, pointB.x, pointB.y, pointC.x, pointC.y)) {
             System.out.println("Точки лежат на одной прямой");
-        }else{
+        } else {
             System.out.println("Точки не лежат на одной прямой");
         }
 
     }
 
-    public static boolean pointToLine(int xA, int yA, int xB, int yB, int xC, int yC){
+    public static boolean pointToLine(int xA, int yA, int xB, int yB, int xC, int yC) {
         return (xC - xA) / (xB - xA) == (yC - yA) / (yB - yA);
     }
 
-    public static class Point{
+
+    class Point {
         int x;
         int y;
-         public Point(int x, int y){
-             this.x = x;
-             this.y = y;
-         }
-    }
 
+        public Point(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+    }
 }
+
+
