8609e4f59067f1f87703db642cb0ad9c936a4cd8
Kiryl Shablinski <kirya-0029@mail.ru>
2022-02-19 09:39:07 +0300

    Task2.2

--- src/Basics_of_software_code/branching/Task2.java
+++ src/Basics_of_software_code/branching/Task2.java
@@ -8,10 +8,32 @@ public class Task2 {
     public static void main(String[] args) {
        Task2 solution = new Task2();
         double[] numbers = solution.createNumbers();
+        double max = 0;
+        double min1 = 0;
+        double min2 = 0;
+        if(numbers[0] < numbers[1]){
+            min1 = numbers[0];
+        }else{
+            min1 = numbers[1];
+        }
+        if (numbers[2] < numbers[3]){
+            min2 = numbers[2];
+        }else{
+            min2 = numbers[3];
+        }
+            if (min1 > min2){
+                max = min1;
 
+            }else{
+                max = min2;
+
+            }
+        System.out.println(max);
+//----------------------------------------
+// or
+// ---------------------------------------
         System.out.println(Math.max(Math.min(numbers[0], numbers[1]),
                 Math.min(numbers[2], numbers[3])));
-
     }
 
     public double[] createNumbers(){
